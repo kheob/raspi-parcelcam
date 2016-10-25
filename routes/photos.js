31919e088125a545a98e9959db39ae99b0c3c52c
Bai Chan Kheo <kheob@hotmail.com>
2016-10-25 16:59:05 +1100

    test

--- routes/photos.js
+++ routes/photos.js
@@ -24,17 +24,12 @@ router.get('/', function(req, res) {
 // Returns images for a particular date (format: YYYY-MM-DD)
 router.get('/:date', function(req, res) {
     var date = new Date(req.params.date); // Start range
-    var endOfDay = new Date(req.params.date); // End range
-
-    date = new Date().setHours(0,0,0,0);
-    endOfDay = new Date().setHours(23,59,59,999);
-
-    // endOfDay.setHours(23,59,59,999); // Source: http://stackoverflow.com/a/8636674/6601606
+    var nextDay = date + 1; // End range
 
     // Find the photos in that date range
     Photo.find({date: {
         "$gte": date,
-        "$lt": endOfDay
+        "$lt": nextDay
     }}, function(err, photos) {
         if (err) {
             return res.status(500).json({message: err.message});
