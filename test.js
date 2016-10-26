99eb88a51f087fa70da4531ce81b6d98b936a527
Bai Chan Kheo <kheob@hotmail.com>
2016-10-26 17:10:53 +1100

    test

--- test.js
+++ test.js
@@ -61,8 +61,7 @@ function startStreaming(io) {
         return;
     }
 
-    // var args = ["-w", "640", "-h", "480", "-o", "./stream/image_stream.jpg", "-t", "999999999", "-tl", "100"];
-    var args = ['-w', '640', '-h', '480', '-o', "./stream/image_stream.jpg", '-t', '20', '-q', '20'];
+    var args = ["-w", "640", "-h", "480", "-o", "./stream/image_stream.jpg", "-t", "999999999", "-tl", "100"];
     proc = spawn('raspistill', args);
 
     console.log('Watching for changes...');
