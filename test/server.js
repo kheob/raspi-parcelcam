292deafff1ff63936b976da45cf1b3a069cf39d8
Bai Chan Kheo <kheob@hotmail.com>
2016-10-26 20:50:29 +1100

    test

--- test/server.js
+++ test/server.js
@@ -32,37 +32,39 @@ http.createServer(function(req, res) {
             // We will undoubtedly have to drop some frames, so create a cache of our last good buffer.
             var lastGoodBuffer;
             python.stdout.on('data', function (data) {
-                // If it's the very first time we receive data, throw the data in the array since
-                // we know it will always start with JPEG header info.
-                if (i == 0) {
-                    bufArray.push(data);
-                    i++;
-                    // Read a few bytes and make sure they're our JPEG header info:
-                    // ff d8 ff e0 00 10 4a 46 49 46 00 01 02 00 00 01 00 01 00 00 ff db 00 etc..
-                } else if (data.readIntLE(0, 1).toString() == "-1" && data.readIntLE(1, 2).toString() == "-40") {
-                    // If our buffer is longer than one, then we have a good frame. Else, we'll
-                    // drop the frame and send our last good frame.
-                    if (bufArray.length > 1) {
-                        // Concatenate the buffers in the array.
-                        var buf = Buffer.concat(bufArray);
-                        // Update cache.
-                        lastGoodBuffer = Buffer(buf);
-                        // Send the frame!
-                        reqHandler.update(buf);
-                        // Start new array.
-                        bufArray = [data];
-                    } else {
-                        // We had an invalid frame, start a new array.
-                        bufArray = [data];
-                        if (lastGoodBuffer != undefined) {
-                            // Send our cached frame!
-                            reqHandler.update(buf);
-                        }
-                    }
-                    // Assembling the JPEG...
-                } else {
-                    bufArray.push(data);
-                }
+                console.log(data);
+
+                // // If it's the very first time we receive data, throw the data in the array since
+                // // we know it will always start with JPEG header info.
+                // if (i == 0) {
+                //     bufArray.push(data);
+                //     i++;
+                //     // Read a few bytes and make sure they're our JPEG header info:
+                //     // ff d8 ff e0 00 10 4a 46 49 46 00 01 02 00 00 01 00 01 00 00 ff db 00 etc..
+                // } else if (data.readIntLE(0, 1).toString() == "-1" && data.readIntLE(1, 2).toString() == "-40") {
+                //     // If our buffer is longer than one, then we have a good frame. Else, we'll
+                //     // drop the frame and send our last good frame.
+                //     if (bufArray.length > 1) {
+                //         // Concatenate the buffers in the array.
+                //         var buf = Buffer.concat(bufArray);
+                //         // Update cache.
+                //         lastGoodBuffer = Buffer(buf);
+                //         // Send the frame!
+                //         reqHandler.update(buf);
+                //         // Start new array.
+                //         bufArray = [data];
+                //     } else {
+                //         // We had an invalid frame, start a new array.
+                //         bufArray = [data];
+                //         if (lastGoodBuffer != undefined) {
+                //             // Send our cached frame!
+                //             reqHandler.update(buf);
+                //         }
+                //     }
+                //     // Assembling the JPEG...
+                // } else {
+                //     bufArray.push(data);
+                // }
             });
 
             python.stderr.on('data', function (data) {
