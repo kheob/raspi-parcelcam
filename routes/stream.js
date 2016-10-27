5c3f0f087d74931c5d782ea0d2f4d63e2167e958
Bai Chan Kheo <kheob@hotmail.com>
2016-10-27 17:04:32 +1100

    test

--- routes/stream.js
+++ routes/stream.js
@@ -16,9 +16,13 @@ router.get('/', function(req, res) {
 
     if (start) {
         // Start the camera (Uses MJPG-Streamer adapted from: https://blog.miguelgrinberg.com/post/how-to-build-and-run-mjpg-streamer-on-the-raspberry-pi)
-        var args = ['--nopreview', '-w', '320', '-h', '240', '-q', '5', '-o', 'stream/stream.jpg', '-tl', '100', '-t', '9999999', '-th', '0:0:0', '&'];
+        var args = ['--nopreview', '-w', '320', '-h', '240', '-q', '5', '-o', 'stream/stream.jpg', '-tl', '100', '-t', '9999999', '-th', '0:0:0'];
         var camera = child_process.spawn('raspistill', args);
-        var liveStream = child_process.spawn('mjpg_streamer', ['-i', 'input_file.so -f stream -n stream.jpg', '-o', 'output_http.so -w /usr/local/www']);
+
+        setInterval(function() {
+            var liveStream = child_process.spawn('mjpg_streamer', ['-i', 'input_file.so -f stream -n stream.jpg', '-o', 'output_http.so -w /usr/local/www']);
+        }, 1000);
+
         res.send('start');
     } else {
         // Stop the camera
