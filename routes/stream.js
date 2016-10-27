3525f510deab9e7f80f5975b7e51690b20a65713
Bai Chan Kheo <kheob@hotmail.com>
2016-10-27 16:44:15 +1100

    test

--- routes/stream.js
+++ routes/stream.js
@@ -19,9 +19,10 @@ router.get('/', function(req, res) {
         var args = ['--nopreview', '-w', '320', '-h', '240', '-q', '5', '-o', 'stream/stream.jpg', '-tl', '100', '-t', '9999999', '-th', '0:0:0', '&'];
         var camera = child_process.spawn('raspistill', args);
         var liveStream = child_process.spawn('LD_LIBRARY_PATH=/usr/local/lib mjpg_streamer', ['-i', 'input_file.so -f /tmp/stream -n pic.jpg', '-o', 'output_http.so -w /usr/local/www']);
+        res.send('start');
     } else {
         // Stop the camera
-
+        res.send('stop');
     }
 });
 
