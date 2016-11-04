82369f42ce2b73c88e20dad0d1167610b3048e67
Bai Chan Kheo <kheob@hotmail.com>
2016-11-04 14:24:51 +1100

    test

--- routes/devices.js
+++ routes/devices.js
@@ -13,32 +13,28 @@ var Device = require('../models/device');
 
 // Submit a device token
 router.post('/', function(req, res) {
-    res.send('bye');
+    // Get the token
+    var token = req.body.token;
 
-    // // Get the token
-    // var token = req.body.token;
-    //
-    // // Add to database
-    // Device.create({token: token}, function(err) {
-    //     if (err) {
-    //         return res.status(500).json({message: err.message});
-    //     }
-    //     // Send response
-    //     res.json({message: 'Success! Device added to push notifications.'})
-    // });
+    // Add to database
+    Device.create({token: token}, function(err) {
+        if (err) {
+            return res.status(500).json({message: err.message});
+        }
+        // Send response
+        res.json({message: 'Success! Device added to push notifications.'})
+    });
 });
 
 // Returns all device tokens
 router.get('/', function(req, res) {
+    Device.find({}, function(err, devices) {
+        if (err) {
+            return res.status(500).json({message: err.message});
+        }
+        res.json({devices: devices});
+    });
     res.send('hi');
-
-    // Device.find({}, function(err, devices) {
-    //     if (err) {
-    //         return res.status(500).json({message: err.message});
-    //     }
-    //     res.json({devices: devices});
-    // });
-    // res.send('hi');
 });
 
 module.exports = router;
